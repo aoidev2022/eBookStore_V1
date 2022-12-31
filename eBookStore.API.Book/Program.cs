6dc7d5eb42bf53f88056a779cec5711f7129cf8b
adonis cruz villarraga <aoi.dev@hotmail.com>
2022-12-31 18:35:55 -0500

    removed autogenerated docker compose support files.

--- eBookStore.API.Book/Program.cs
+++ eBookStore.API.Book/Program.cs
@@ -8,8 +8,7 @@ namespace eBookStore.API.Book
     public class Program
     {
         public static void Main(string[] args)
-        {
-            Console.WriteLine($"eBookStore.API.Book <-> these are my args [{string.Join(",", args)}]");
+        {            
             CreateHostBuilder(args).Build().Run();
         }
 
